70df6004d6782df7e36bd69ec5127b19a5155c95
Tomislav Kescec <tomislav.kescec@gmail.com>
2022-06-29 21:25:46 +0200

    Složio page s postavkama za knjižaru u admin sučelju.

---
+++ Knjižara/Areas/Admin/Models/SettingsViewModels.cs
@@ -0,0 +1,30 @@
+﻿using Knjižara.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Knjižara.Areas.Admin.Models
+{
+    public class SettingsViewModel
+    {
+        public int Idknjizara { get; set; }
+
+        [Required]
+        public string? Naziv { get; set; }
+
+        [Required]
+        public string? Adresa { get; set; }
+
+        [Required]
+        [Oib]
+        public string? Oib { get; set; }
+
+        [Required]
+        public string? Iban { get; set; }
+
+        [Required]
+        public string? Logo { get; set; }
+
+        public string? UvjetiKoristenja { get; set; }
+
+        public bool Success { get; set; }
+    }
+}
